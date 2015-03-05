f7128f2c7a0a585bf2fabcafc46910907c4128dc
sionqi <sion.qi@missionsky.com>
2015-03-05 10:10:11 +0800

    update

---
+++ bin/rebuild.sh
@@ -0,0 +1,2 @@
+/usr/lib/ckan/default/bin/paster --plugin=ckan tracking update -c /etc/ckan/default/production.ini
+/usr/lib/ckan/default/bin/paster --plugin=ckan search-index rebuild -r -c /etc/ckan/default/production.ini
